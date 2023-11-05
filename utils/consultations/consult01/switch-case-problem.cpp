447b10cbfe91062da0a47dda28742016fad83f78
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2023-11-05 16:41:52 +0200

    Create switch-case-problem.cpp

---
+++ utils/consultations/consult01/switch-case-problem.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    /*
+    Rewrite the following code using switch statement:
+    int a;
+    cin >> a;
+    if (a != 1) cout << “isn`t one”;
+    if ((a >= 2) && (a <= 4)) cout << “between two & four”;
+    if (a != 4) cout << “isn`t four”;
+    */
+  
+    int a;
+    cin >> a;
+    switch (a)
+    {
+      case 1: cout << "isn`t four " << endl; break;
+      case 2:
+      case 3: cout << "isn`t one " << "between " << "isn`t four" << endl; break;
+      case 4: cout << "isn`t one " << "between " << endl; break;
+      default: cout << "isn`t one " << "isn`t four " << endl; break;
+    }
+}
