7ad81f885436a1be4a056335fe057870e3c8d583
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2023-11-23 11:27:27 +0200

    Create PlusOne.cpp

---
+++ utils/solutions/PlusOne.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+//max counting system
+const int MAX_K_VALUE = 16;
+
+//helper functions
+int getIndexFromChar(char s)
+{
+    if (s >= '0' && s <= '9')
+        return s - '0';
+
+    if (s >= 'A' && s <= 'F')
+        return s - 'A' + 10;
+
+    return -1;
+}
+
+char getCharFromIndex(int index)
+{
+    if (index >= 0 && index <= 9)
+        return index + '0';
+
+    if (index >= 10 && index <= 15)
+        return index + 'A' + 10;
+
+    return 0;
+}
+
+bool canBeIncremented(int index, int k)
+{
+    return index != k - 1;
+}
+
+bool isValidDigitInK(char digit, int k)
+{
+    int index = getIndexFromChar(digit);
+    return (index != -1 && index < k);
+}
+
+bool isValidInput(const char arr[], size_t size, int k)
+{
+    if (k > MAX_K_VALUE)
+        return false;
+
+    for (int i = 0; i < k; i++)
+    {
+        if (!isValidDigitInK(arr[i], k))
+            return false;
+
+    }
+
+    return true;
+}
+
+//plusOne solution
+void plusOne(char arr[], size_t size, int k)
+{
+    if (!isValidInput(arr, size, k))
+        return; // do nothing
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        int index = getIndexFromChar(arr[i]);
+        if (!canBeIncremented(index, k))
+        {
+            arr[i] = '0';
+        }
+        else
+        {
+            index++;
+            arr[i] = getCharFromIndex(index);
+            break;
+        }
+    }
+}
